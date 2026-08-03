4e76e7f02e7e8dcc4d8061eb088848c5072acbe1
roberioferreira81-TCC <roberio.ferreira81@gmail.com>
2026-08-03 14:16:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const poolConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  Timezone: 'z',
   waitForConnections: true,
   connectionLimit: 10,
 };
